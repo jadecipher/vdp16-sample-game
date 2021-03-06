78b422519ebd5da2a0d995454f7013e82b3f68b3
jadecipher <88575488+jadecipher@users.noreply.github.com>
2021-03-06 10:52:40 +0000

    Update Main.js

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,1 +1,8 @@
-import Rea
+import React from 'react';
+
+export const Home = () => {
+  return (
+    <div className="body-container">
+      <div className="my-photo">
+        <img src="michaela_linkedIn_photo.jpg" alt="Photo of Me" width="100%" />
+      </div
