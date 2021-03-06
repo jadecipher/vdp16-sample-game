70d66800fbd7e753c791bfaf58ea44462dad4fed
jadecipher <88575488+jadecipher@users.noreply.github.com>
2021-03-06 16:38:52 +0000

    Refactoring changes

--- client/components/Home.js
+++ client/components/Home.js
@@ -5,4 +5,8 @@ export const Home = () => {
     <div className="body-container">
       <div className="my-photo">
         <img src="michaela_linkedIn_photo.jpg" alt="Photo of Me" width="100%" />
-      </div
+      </div>
+      <div id="bullets">
+        <h4>Saint Michael's College Alumn</h4>
+        <h4>Former Calculus Teacher</h4>
+        <h4>Grace Hopper Program at Fullstack Academy of Code Alum
