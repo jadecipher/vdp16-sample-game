31029895098a5b886a7993e18c435021ec4a83b7
jadecipher <88575488+jadecipher@users.noreply.github.com>
2021-03-23 11:20:16 +0000

    renamings

--- client/components/LeftNav.js
+++ client/components/LeftNav.js
@@ -23,4 +23,4 @@ const LeftNav = () => (
     >
       <img
         className="logo"
-        src="https://c
+        src="https://camo.githubusercontent.com/7710b43d0476b6f6d4b4b2865e35c108f69991f3/68747470733a2f2f7777772e69636f6e66696e6465722e636f6d2f646174612f69636f6e732f6f637469636f6e732f313032342f6d61726b2d67697
