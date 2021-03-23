274dc95949fca4055af485c74e6d23c82a85bfb0
jadecipher <88575488+jadecipher@users.noreply.github.com>
2021-03-23 17:30:14 +0000

    5.0

--- client/components/LeftNav.js
+++ client/components/LeftNav.js
@@ -23,4 +23,12 @@ const LeftNav = () => (
     >
       <img
         className="logo"
-        src="https://camo.githubusercontent.com/7710b43d0476b6f6d4b4b2865e35c108f69991f3/68747470733a2f2f7777772e69636f6e66696e6465722e636f6d2f646174612f69636f6e732f6f637469636f6e732f313032342f6d61726b2d67697
+        src="https://camo.githubusercontent.com/7710b43d0476b6f6d4b4b2865e35c108f69991f3/68747470733a2f2f7777772e69636f6e66696e6465722e636f6d2f646174612f69636f6e732f6f637469636f6e732f313032342f6d61726b2d6769746875622d3235362e706e67"
+        alt="GitHub icon"
+      />
+    </a>
+    <a
+      href="mailto:michaelaburns0@gmail.com?Subject=Hi%20Michaela!"
+      target="_blank"
+      without
+      r
