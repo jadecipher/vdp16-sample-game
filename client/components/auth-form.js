a7380b05a3857181d1fe24528f12f40599bd861e
jadecipher <88575488+jadecipher@users.noreply.github.com>
2021-03-24 09:19:22 +0000

    v0.9.11

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -1,2 +1,10 @@
 import React from 'react';
-import {
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+import {auth} from '../store';
+
+/**
+ * COMPONENT
+ */
+const AuthForm = props => {
+  const {name, displayName, han
