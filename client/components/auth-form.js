a7bd4e99730a0ae1f5112e4cc392ec07db2261c4
jadecipher <88575488+jadecipher@users.noreply.github.com>
2021-03-24 22:17:36 +0000

    Version bump to 0.7.2

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -22,4 +22,9 @@ const AuthForm = props => {
           <label htmlFor="password">
             <small>Password</small>
           </label>
-          
+          <input name="password" type="password" />
+        </div>
+        <div>
+          <button type="submit">{displayName}</button>
+        </div>
+        {error && error.response && <div> {error.
