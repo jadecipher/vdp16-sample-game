02c4a08796300eb0103c21e3b27bc54d9ae99440
jadecipher <88575488+jadecipher@users.noreply.github.com>
2021-04-17 10:16:16 +0000

    phone number

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,4 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
-import {Link} fro
+import {Link} from 'react-router-dom';
+
+/**
+ * COMPONENT
+ */
+export const UserHome = props => {
+  const {email} = props;
+
+  return (
+    <div>
+      <h3>Welcome, {email}</h3>
+      <Link to="/admin">
