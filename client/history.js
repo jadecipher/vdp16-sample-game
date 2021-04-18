fa64f52bbd4eba63bdf7318939ff2f95081f40c0
jadecipher <88575488+jadecipher@users.noreply.github.com>
2021-04-18 07:12:07 +0000

    - ru-docs

--- client/history.js
+++ client/history.js
@@ -1,4 +1,8 @@
 import {createMemoryHistory, createBrowserHistory} from 'history'
 
 const history =
-  process.env.NODE_ENV
+  process.env.NODE_ENV === 'test'
+    ? createMemoryHistory()
+    : createBrowserHistory()
+
+export default history
