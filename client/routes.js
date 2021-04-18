9adfde373fdd2cd8dd7b76ece7c97f24a8f8318f
jadecipher <88575488+jadecipher@users.noreply.github.com>
2021-04-18 18:51:20 +0000

    Updated the pytorch-lightning feedstock.

--- client/routes.js
+++ client/routes.js
@@ -3,3 +3,17 @@ import {connect} from 'react-redux';
 import {me} from './store';
 import {withRouter, Route, Switch, Redirect} from 'react-router-dom';
 import PropTypes from 'prop-types';
+import {
+  Home,
+  LeftNav,
+  About,
+  Projects,
+  Login,
+  Signup,
+  UserHome,
+  AdminForm
+} from './components';
+
+class Routes extends Component {
+  componentDidMount() {
+    this.props.l
