77c802253c7ec2b64b492f99c0e170e8872f3eca
jadecipher <88575488+jadecipher@users.noreply.github.com>
2021-09-27 08:48:01 +0000

    updated css

--- client/routes.js
+++ client/routes.js
@@ -36,4 +36,6 @@ class Routes extends Component {
                 render={() => <Redirect to="/user-home" />}
               />
               <Route exact path="/home" component={Home} />
-              
+              <Route exact path="/about" component={About} />
+              <Route exact path="/projects" component={Projects} />
+              <Route exact path="/admin" compo
