6086965da17bd4deb9d487ed74a401bb61ac0415
jadecipher <88575488+jadecipher@users.noreply.github.com>
2021-10-13 20:24:36 +0000

    Add sample config file

---
+++ client/store/user.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+import history from '../history'
+
+/**
+ * ACTION TYPES
+ */
+const GET_USER = 'GET_USER'
+const REMOVE_USER = 'REMOVE_USER'
+
+/**
+ * INITIAL STATE
+ */
+const def
