e13611905b25b4289865e670517297b9e530d3c8
jadecipher <88575488+jadecipher@users.noreply.github.com>
2021-12-31 22:44:40 +0000

    added gitattributes

--- public/bundle.js
+++ public/bundle.js
@@ -213,3 +213,193 @@ var Home = function Home() {
   }, "The Grace Hopper Program"), ' ', "at Fullstack Academy of Code")));
 };
 /* harmony default export */ __webpack_exports__["default"] = (Home);
+
+/***/ }),
+
+/***/ "./client/components/LeftNav.js":
+/*!**************************************!*\
+  !*** ./client/components/LeftNav.js ***!
+  \**************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+
+
+var LeftNav = function LeftNav() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    id: "left-nav"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    href: "https://www.linkedin.com/in/burnsmich/",
+    target: "_blank",
+    without: true,
+    rel: "noopener noreferrer"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    className: "logo",
+    src: "https://upload.wikimedia.org/wikipedia/commons/c/ca/LinkedIn_logo_initials.png",
+    alt: "linkedin icon"
+  })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    color: "inherit",
+    href: "https://github.com/michslush",
+    target: "_blank",
+    without: true,
+    rel: "noopener noreferrer"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    className: "logo",
+    src: "https://camo.githubusercontent.com/7710b43d0476b6f6d4b4b2865e35c108f69991f3/68747470733a2f2f7777772e69636f6e66696e6465722e636f6d2f646174612f69636f6e732f6f637469636f6e732f313032342f6d61726b2d6769746875622d3235362e706e67",
+    alt: "GitHub icon"
+  })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    href: "mailto:michaelaburns0@gmail.com?Subject=Hi%20Michaela!",
+    target: "_blank",
+    without: true,
+    rel: "noopener noreferrer"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    className: "logo",
+    src: "https://upload.wikimedia.org/wikipedia/commons/4/4e/Gmail_Icon.png",
+    alt: "email icon"
+  })));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (LeftNav);
+
+/***/ }),
+
+/***/ "./client/components/Projects.js":
+/*!***************************************!*\
+  !*** ./client/components/Projects.js ***!
+  \***************************************/
+/*! exports provided: Projects, default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Projects", function() { return Projects; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+
+
+var CodeCapstone = function CodeCapstone() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "individual-project"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", null, "Code Aloud"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "project-content"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h4", null, "Code Aloud is a technical interview preparation application focusing on the approach to a coding challenge, and how to communicate that approach before coding begins. We offer speech-to-text functionality to simulate aspects of interviewing, as well as a code sandbox with tests built in so users can check their work."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h4", null, "My Responsibilities: Created and imported interactive line graph feature using Recharts. Developed front-end components using JavaScript and React. Deployed website via Heroku and enabled continuous integration with GitHub via Travis CI."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h4", null, "View the deployed site at", ' ', react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    color: "inherit",
+    href: "https://codealoud.herokuapp.com/",
+    target: "_blank",
+    without: true,
+    rel: "noopener noreferrer"
+  }, "www.codealoud.gq")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h4", null, "View the GitHub repo", ' ', react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    color: "inherit",
+    href: "https://github.com/codecapstone/codealoud",
+    target: "_blank",
+    without: true,
+    rel: "noopener noreferrer"
+  }, "HERE")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h4", null, "Technology Used: Node, Express, PostgreSQL, Sequelize, React, Redux, Annyang, Recharts, CodeSandbox, Jest, Mocha, Chai, Heroku, Travis CI"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h4", null, "Teammates: Allison French, Jennifer Grange, Rashmi Prakash"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("iframe", {
+    width: "560",
+    height: "315",
+    src: "https://www.youtube.com/embed/nZpv5FfNUeI",
+    frameBorder: "0",
+    allow: "accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture",
+    allowFullScreen: true
+  })));
+};
+
+var GraceShopper = function GraceShopper() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "individual-project"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", null, "Cloud Nine"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "project-content"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h4", null, "Cloud Nine is an e-commerce website, designed to sell nine different types of air. Some features of the site include sign-up and Google O-Auth, shopping carts unique to each user, and checkout."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h4", null, "My Responsibilities: Developed front-end components and back-end routes to display the store\u2019s inventory, shopping cart, and checkout feature using JavaScript, Sequelize, Express, React, and Redux; Created tests for component, models, and API functionality using Mocha and Chai"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h4", null, "View the deployed site at", ' ', react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    color: "inherit",
+    href: "https://cl0udnine.herokuapp.com/",
+    target: "_blank",
+    without: true,
+    rel: "noopener noreferrer"
+  }, "cl0udnine.com")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h4", null, "View the GitHub repo", ' ', react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    color: "inherit",
+    href: "https://github.com/team-myrel/cl0udnine",
+    target: "_blank",
+    without: true,
+    rel: "noopener noreferrer"
+  }, "HERE")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h4", null, "Technology Used: Node, Express, PostgreSQL, Sequelize, React, Redux, Mocha, Chai, Heroku, Travis CI"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h4", null, "Teammates: Rashmi Prakash, Lerena Holloway, Lisa Jakubczyk"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
+    src: "cloudnine.png",
+    alt: "Cloud Nine",
+    width: "100%"
+  })));
+};
+
+var Projects = function Projects() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    id: "projects-container",
+    className: "body-container"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "Projects"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    id: "projects"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(CodeCapstone, null), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(GraceShopper, null)));
+};
+/* harmony default export */ __webpack_exports__["default"] = (Projects);
+
+/***/ }),
+
+/***/ "./client/components/auth-form.js":
+/*!****************************************!*\
+  !*** ./client/components/auth-form.js ***!
+  \****************************************/
+/*! exports provided: Login, Signup */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Login", function() { return Login; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Signup", function() { return Signup; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react-redux */ "./node_modules/react-redux/es/index.js");
+/* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! prop-types */ "./node_modules/prop-types/index.js");
+/* harmony import */ var prop_types__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(prop_types__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _store__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../store */ "./client/store/index.js");
+
+
+
+
+/**
+ * COMPONENT
+ */
+
+var AuthForm = function AuthForm(props) {
+  var name = props.name,
+      displayName = props.displayName,
+      handleSubmit = props.handleSubmit,
+      error = props.error;
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("form", {
+    onSubmit: handleSubmit,
+    name: name
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("label", {
+    htmlFor: "email"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("small", null, "Email")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
+    name: "email",
+    type: "text"
+  })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("label", {
+    htmlFor: "password"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("small", null, "Password")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("input", {
+    name: "password",
+    type: "password"
+  })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
+    type: "submit"
+  }, displayName)), error && error.response && react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, " ", error.response.data, " ")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("a", {
+    href: "/auth/google"
+  }, displayName, " with Google"));
+};
+/**
+ * CONTAINER
+ *   Note that we have two different sets of 'mapStateToProps' functions -
+ *   one for Login, and one for Signup. However, they share the same 'mapDispatchToProps'
+ *   function, and share the same Component. This is a good example of how we
+ *   can stay DRY with interfaces that are very similar to each other!
+ */
+
+
+var mapLogin = function mapLogin(state) {
