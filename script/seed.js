6a0a74749fe0ca09b9a1f1cf4e215ed986e20b16
jadecipher <88575488+jadecipher@users.noreply.github.com>
2022-07-24 17:37:36 +0000

    Fixed incorrect comment.

--- script/seed.js
+++ script/seed.js
@@ -1,3 +1,10 @@
 'use strict';
 
-const db = re
+const db = require('../server/db');
+const {Project} = require('../server/db/models');
+
+async function seed() {
+  await db.sync({force: true});
+  console.log('db synced!');
+
+  const projects
