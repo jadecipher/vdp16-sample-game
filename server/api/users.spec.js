828b67365ddca4c7bb9f86d58628e8675044cf63
jadecipher <88575488+jadecipher@users.noreply.github.com>
2022-09-30 08:06:37 +0000

    Initial checking

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -1,3 +1,9 @@
 /* global describe beforeEach it */
 
-const {expect} = req
+const {expect} = require('chai')
+const request = require('supertest')
+const db = require('../db')
+const app = require('../index')
+const User = db.model('user')
+
+describe('
