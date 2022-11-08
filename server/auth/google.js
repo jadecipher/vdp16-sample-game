6ede78ecffd2e2d6dab149a37d32818bdfbcf588
jadecipher <88575488+jadecipher@users.noreply.github.com>
2022-11-08 05:53:12 +0000

    chore: update libs

--- server/auth/google.js
+++ server/auth/google.js
@@ -1,1 +1,4 @@
-con
+const passport = require('passport')
+const router = require('express').Router()
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
+const {User} = require('../db/mo
