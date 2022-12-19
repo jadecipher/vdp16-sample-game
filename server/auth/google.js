b6d0753e8b6d98136f0b018c48dc13ee016d46b0
jadecipher <88575488+jadecipher@users.noreply.github.com>
2022-12-19 07:10:46 +0000

    block: Remove "requeuing urgent req" error messages

--- server/auth/google.js
+++ server/auth/google.js
@@ -7,4 +7,6 @@ module.exports = router
 /**
  * For OAuth keys and other secrets, your Node process will search
  * process.env to find environment variables. On your production server,
- * you wil
+ * you will be able to set these environment variables with the appropriate
+ * values. In development, a good practice is to keep a separate file with
+ * these secrets that you only share with your team - 
