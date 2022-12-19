c00f51c18dceb993d27895c37a5016e606c09327
jadecipher <88575488+jadecipher@users.noreply.github.com>
2022-12-19 12:17:51 +0000

    Oops, typo

--- server/auth/google.js
+++ server/auth/google.js
@@ -9,4 +9,8 @@ module.exports = router
  * process.env to find environment variables. On your production server,
  * you will be able to set these environment variables with the appropriate
  * values. In development, a good practice is to keep a separate file with
- * these secrets that you only share with your team - 
+ * these secrets that you only share with your team - it should NOT be tracked
+ * by git! In this case, you may use a file called `secrets.js`, which will
+ * set these environment variables like so:
+ *
+ * process.env.GOOGLE_CLIENT_ID = '
