6bc16620bab930c6623a519e6cb653c994718984
jadecipher <88575488+jadecipher@users.noreply.github.com>
2022-12-24 05:31:45 +0000

    [l10n] Updated Catalan translation

--- server/auth/google.js
+++ server/auth/google.js
@@ -22,4 +22,11 @@ if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
   console.log('Google client ID / secret not found. Skipping Google OAuth.')
 } else {
   const googleConfig = {
-    clientID: process.env
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
+  }
+
+  const strategy = new GoogleStrategy(
+    googleConfig,
+   
