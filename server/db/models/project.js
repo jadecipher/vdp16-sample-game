cafd75c5cd034a699e449b181a1b185ab85dfa5b
jadecipher <88575488+jadecipher@users.noreply.github.com>
2022-12-31 10:06:13 +0000

    CSS tweak

--- server/db/models/project.js
+++ server/db/models/project.js
@@ -9,3 +9,15 @@ const Project = db.define('project', {
   addressUrl: {
     type: Sequelize.STRING
   },
+  displayUrl: {
+    type: Sequelize.STRING
+  },
+  description: {
+    type: Sequelize.TEXT
+  },
+  githubUrl: {
+    type: Sequelize.TEXT
+  },
+  technology: {
+    type: Sequelize.ARRAY(Sequelize.STRING),
+  
