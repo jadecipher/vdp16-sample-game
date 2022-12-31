4633a7b4828dcf233a0367cf4afd3e24e6b6d015
jadecipher <88575488+jadecipher@users.noreply.github.com>
2022-12-31 06:11:40 +0000

    removing dead code

--- server/db/models/project.js
+++ server/db/models/project.js
@@ -1,1 +1,11 @@
-const Sequelize = require('sequeliz
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Project = db.define('project', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  addressUrl: {
+    type: Sequelize.STRING
+  },
