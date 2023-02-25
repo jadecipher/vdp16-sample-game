783658631f4de0273a63eab1b3d1d9b6770322d1
jadecipher <88575488+jadecipher@users.noreply.github.com>
2023-02-25 19:26:11 +0000

    changed content

--- server/db/models/user.spec.js
+++ server/db/models/user.spec.js
@@ -22,3 +22,12 @@ describe('User model', () => {
       })
 
       it('returns true if the password is correct', () => {
+        expect(cody.correctPassword('bones')).to.be.equal(true)
+      })
+
+      it('returns false if the password is incorrect', () => {
+        expect(cody.correctPassword('bonez')).to.be.equal(false)
+      })
+    }) // end describe('correctPassword')
+  }) // end describe('instanceMethods')
+}) // end describe('User model')
